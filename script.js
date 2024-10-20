541391f6b12417f7c5e58f3968488944188abf3c
Ismael Alonso Garcia <eva.santana.deniz@gmail.com>
2024-10-20 13:34:06 +0100

    menu extensible creado, faltan detalles en el styling de la lista

---
+++ script.js
@@ -0,0 +1,29 @@
+// NAVBAR 
+
+//DESPLIEGUE DEL MENÚ LATERAL
+
+const navbar = document.getElementById('navbar');
+const menuButton = document.getElementById('menuButton');
+const menuContent = document.getElementById('menuContent');
+
+//función para abrir y cerrar el menú
+menuButton.addEventListener('click', function () {
+    menuContent.classList.toggle('active');
+});
+
+// Función para cerrar el menú si se hace clic fuera de él
+document.addEventListener('click', function (event) {
+    let isClickInsideMenu = navbar.contains(event.target);
+
+    if (!isClickInsideMenu) {
+        menuContent.classList.remove('active');
+    }
+});
+
+// Cerrar el menú al hacer clic en un enlace dentro del menú
+let menuLinks = document.querySelectorAll('#menuContent a');
+menuLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+        menuContent.classList.remove('active');
+    });
+});
